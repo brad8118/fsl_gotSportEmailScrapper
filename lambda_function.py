a574e9a0c0438a39804d302f001c0070c911dd9e
Brad <brad8118@gmail.com>
2025-01-05 21:26:59 -0500

    Added header when reading html, export for google

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import requests
 # from botocore.vendored import requests
 from bs4 import BeautifulSoup
 import json
+import re
 
 #lambda function
 def lambda_handler(event, context):
@@ -16,13 +17,13 @@ def lambda_handler(event, context):
   #   url = "https://system.gotsport.com/org_event/events/18280/schedules?club=3694"    
   #   print("Using default URL", url)
 
-  url = "https://system.gotsport.com/org_event/events/18280/clubs" 
+  url = "https://system.gotsport.com/org_event/events/36230/clubs" 
   details = getCoachDetails(url)
   exportToCVS(details)
   return details
   
 def getCoachDetails(url):
-  url = "https://system.gotsport.com/org_event/events/18280/clubs" 
+  # url = "https://system.gotsport.com/org_event/events/18280/clubs" 
   contacts = []
   
   html =  getHtml(url)
@@ -32,9 +33,14 @@ def getCoachDetails(url):
     teamsByClubHtml = getHtml(club['link'])
     clubContacts = parseContactDetailsFromClubHtml(teamsByClubHtml)
     for c in clubContacts:
-      c['club_name'] = club['club_name']
+      c['Company'] = club['club_name']
+      c['Job title'] = re.sub(club['club_name']+ " ", "", c['Job title'], flags=re.I)
+      c['Team Name'] = re.sub(club['club_name']+ " ", "", c['Team Name'], flags=re.I)
+      
     # print('clubContacts', clubContacts)
     contacts.extend(clubContacts)
+
+    # break
 
   # print('LAST CLUB --', contacts[-1])
   return contacts
@@ -44,6 +50,9 @@ def getCoachDetails(url):
 # https://system.gotsport.com/org_event/events/18280/clubs
 def parseClubsListingHtml(html):
   clubs = []
+  # print("-----------------------------------")
+  # print("parseClubsListingHtml")
+  # print(html)
   tableOfClubs = html.find_all("table", {"class": "table table-bordered table-condensed table-hover"})[0]
 
 #loop club links
@@ -56,6 +65,7 @@ def parseClubsListingHtml(html):
     clubs.append({'club_name':aTag.text, 'link': clubLink})
   
   return clubs
+
 
 ## parse html, to get team details then jump 2 deep to get coaches' details
 # https://system.gotsport.com/org_event/events/18280/clubs/18604
@@ -75,8 +85,18 @@ def parseContactDetailsFromClubHtml(html):
     # print('teamContactUrl', teamContactUrl)
     contactHtml =  getHtml(teamContactsUrl)
 
-    # ////
     tableOfPeople = contactHtml.find("table")
+
+    # Since we're generating the link for the contacts and not clicking the teampage from the club page
+    # the team page actually might not a link to the coaches
+    # When a team doesn't have coaches listed, redirected to -> https://system.gotsport.com/org_event/events/36230
+    # kinda lazy but if the default page returned doesn't have a table it in. 
+
+    # print("Is tableOfPeople None, team probably doesn't have coaches listed. ", teamUrl)
+    if tableOfPeople is None:
+      print("Team ", teamCols[0], " doesn't have a team contact listed. ", teamUrl)
+      continue
+
     rowsOfPeople = tableOfPeople.find_all('tr')    
     # rowsOfPeople.pop(0)
     for personRow in rowsOfPeople:
@@ -85,9 +105,41 @@ def parseContactDetailsFromClubHtml(html):
       personCols = [ele.text.strip() for ele in personCols]
       
       # print('personCols', personCols)
-      details = {'team':teamCols[0], 'url': teamUrl, 'gender':teamCols[1], 
-                 'age':teamCols[2],'division': teamCols[3], 'bracket':teamCols[4], 
-                'person_name':personCols[1], 'email':personCols[2], 'cell':personCols[3]}
+      # details = {'team':teamCols[0], 'url': teamUrl, 'gender':teamCols[1], 
+      #            'age':teamCols[2],'division': teamCols[3], 'bracket':teamCols[4], 
+      #           'person_name':personCols[1], 'email':personCols[2], 'cell':personCols[3]}
+      
+      f_name = personCols[1]
+      l_name = ""
+
+      if " " in personCols[1]:
+        f_name = personCols[1][:personCols[1].find(" ")]
+        l_name = personCols[1][personCols[1].find(" "):]
+
+      notes = 'Age: ' + teamCols[2] + ' Division: ' + teamCols[3], ' Bracket: ' + teamCols[4]
+      b_or_g =  "G" if teamCols[1][0].upper() == "F" else "B"
+
+      #import for google 
+      details = {
+                "Team Name": teamCols[0],
+                'Job title':teamCols[0] + " (" + teamCols[2] + " " + b_or_g + ")", 
+                 "Custom Field 1 - Label": "Gender", "Custom Field 1 - Value": teamCols[1], 
+                 "Custom Field 2 - Label": "Age", "Custom Field 2 - Value": teamCols[2], 
+                 "Custom Field 3 - Label": "Division", "Custom Field 3 - Value": teamCols[3], 
+                 "Custom Field 4 - Label": "Bracket", "Custom Field 4 - Value": teamCols[4], 
+
+                 'First Name':f_name, 'Last Name':l_name,
+                 "E-mail Address": personCols[2], 
+                #  "E-mail 1 - Label": "Work", "E-mail 1 - Value": personCols[2],
+                #  "E-mail 1 - Label": personCols[2], "E-mail 1 - Value": "Work",
+                #  "Phone 1 - Label": "Phone", "Phone 1 - Value": personCols[3],
+                #  "Phone 1 - Label": personCols[3], "Phone 1 - Value": "Phone",
+                 "Phone": personCols[3], 
+                #  "Website 1 - Label": "Schedule", "Website 1 - Value": teamUrl,
+                 "Web Page": teamUrl,
+
+                'person_name':personCols[1]}
+                
       # print('details', details)
       contacts.append(details)
 
@@ -97,12 +149,24 @@ def parseContactDetailsFromClubHtml(html):
 def getHtml(url):
   try:
     print("Calling url:", url)
-    webpage = requests.get(url)
+    headers = { 'accept':'*/*',
+      'accept-encoding':'gzip, deflate, br',
+      'accept-language':'en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,la;q=0.6',
+      'cache-control':'no-cache',
+      'dnt':'1',
+      'pragma':'no-cache',
+      'referer':'https',
+      'sec-fetch-mode':'no-cors',
+      'sec-fetch-site':'cross-site',
+      'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+    }
+    webpage = requests.get(url=url, headers=headers)
+    # print("Webpage")
     # print(webpage)
     
     if webpage.status_code == 200:
       webpage_bs = BeautifulSoup(webpage.text, 'html.parser')
-      # webpage_bs = BeautifulSoup(webpage.content, 'html.parser')
+      webpage_bs = BeautifulSoup(webpage.content, 'html.parser')
       # print("Success")
       # print(webpage_bs.prettify())
       # print("Success getting HTML")
@@ -114,10 +178,32 @@ def getHtml(url):
     print("request does not work : ", url)
     print("Exception : ", e)
     raise Exception("API Exception - " + url + " " + e)
+  
 
 def exportToCVS(details):
   with open("contactList.csv","w",newline="") as f:  
-      title = "club_name,team,url,gender,age,division,bracket,person_name,email,cell".split(",") # quick hack
+      # title = "Organization Name,Organization Department,team,url,gender,age,division,bracket,person_name,email,cell".split(",") # quick hack
+      title = [
+               "Company", # club name
+               "Team Name", #team name
+               "Job title",  # team name + Sex + Age
+              #  "Website 1 - Label",  "Website 1 - Value", #URL
+               "Web Page",
+               "Custom Field 1 - Label", "Custom Field 1 - Value", #gender
+               "Custom Field 2 - Label", "Custom Field 2 - Value",  # "age",
+               "Custom Field 3 - Label", "Custom Field 3 - Value",  # "Division",
+               "Custom Field 4 - Label", "Custom Field 4 - Value",  # "Bracket",
+               "person_name", "First Name", "Last Name",
+              #  "E-mail 1 - Label", "E-mail 1 - Value",  #"email"
+               "E-mail Address",
+               "Phone",
+              #  "Phone 1 - Label", "Phone 1 - Value", # cell
+               ]
+      
+      # for d in details:
+      #   print(d)
+      print("Exporting to csv!!")
+      
       cw = csv.DictWriter(f,title,delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
       cw.writeheader()
       cw.writerows(details)
